aaad2a147d2aa42b5534b92fa253d40557579758
alexdotwells <ajwells99@gmail.com>
2018-02-05 23:01:06 -0600

    Solved Day 1 - C#

---
+++ 1 - Data Types/Solution.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Solution {
+    static void Main(String[] args) {
+        // Declare second integer, double, and String variables.
+        int i2 = new int();
+        double d2 = new double();
+        string s2 = "";
+
+        // Read and save an integer, double, and String to your variables.
+        i2 = int.Parse(Console.ReadLine());
+        d2 = double.Parse(Console.ReadLine());
+        s2 = Console.ReadLine();
+
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(i + i2);
+
+        // Print the sum of the double variables on a new line.
+        double dSum = d + d2;
+        Console.WriteLine(dSum.ToString("0.0"));
+
+        // Concatenate and print the String variables on a new line
+        // The 's' variable will printed first.
+        Console.WriteLine(s + s2);
+    }
+}
