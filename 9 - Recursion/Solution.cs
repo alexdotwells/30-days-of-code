7982f5fd4ce7497bbf2e8b13076094ae138f0716
alexdotwells <ajwells99@gmail.com>
2018-02-07 23:34:38 -0600

    Solved Day 9 - C#

---
+++ 9 - Recursion/Solution.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Solution {
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = factorial(n);
+        Console.WriteLine(result);
+    }
+
+    static int factorial(int n) {
+        if (n==1) return 1;
+        return factorial(n-1) * n;     
+    }
+    
+}
